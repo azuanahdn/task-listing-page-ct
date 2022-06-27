789786a3e6c777f5a0e8984c4e823fac1c5159a5
azuanahdn <78471038+azuanahdn@users.noreply.github.com>
2022-06-27 16:41:28 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskList from './Main/TaskList.js';
+
+function App() {
+  return (
+    <div className="App-header">
+        <TaskList />
+    </div>
+  );
+}
+
+export default App;
